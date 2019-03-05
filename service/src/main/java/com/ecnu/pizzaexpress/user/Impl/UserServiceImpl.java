7111d3251ada95c4bc323d08c271ed4994243e24
unknown <1322545725@qq.com>
2019-03-05 19:25:25 +0800

    config

---
+++ service/src/main/java/com/ecnu/pizzaexpress/user/Impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ecnu.pizzaexpress.user.Impl;
+
+import com.ecnu.pizzaexpress.user.IUserService;
+
+public class UserServiceImpl implements IUserService {
+
+    @Override
+    public String login() {
+        return "";
+    }
+
+    @Override
+    public String signup() {
+        return "";
+    }
+}
